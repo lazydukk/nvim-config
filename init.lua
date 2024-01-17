b494d403a463767d2ae0c7a20121f247e7c1ef04
lazydukk <bytex07@proton.me>
2024-01-17 09:55:11 +0530

    lsp config update

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
+PATH = "prepend"
 local opts = {}
 
 require("vim-options")
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,25 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "cssmodules_ls",
+          "cssls",
+          "golangci_lint_ls",
+          "gopls",
+          "html",
+          "jsonls",
+          "biome",
+          "marksman",
+          "prismals",
+          "pyre",
+          "pyright",
+          "tailwindcss",
+          "taplo",
+          "hydra_lsp",
+          "vtsls"
+        }
       })
     end
   },
@@ -18,6 +36,26 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.cssmodules_ls.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.golangci_lint_ls.setup({})
+      lspconfig.gopls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.biome.setup({})
+      lspconfig.marksman.setup({})
+      lspconfig.prismals.setup({})
+      lspconfig.pyre.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.taplo.setup({})
+      lspconfig.hydra_lsp.setup({})
+      lspconfig.vtsls.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
