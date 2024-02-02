8954722ea6feff3fb06066306f9827ac0052a6b7
Banuljith Dasnaka <bytex07@proton.com>
2024-02-02 13:52:46 +0530

    update readme | add plugins

--- README.md
+++ README.md
@@ -2,9 +2,37 @@
 
 just figuring it out :)
 
-- plugins_dir: `./lua/plugins/`
-
-
 ### fixes
-bug: `language server not installed or missing from path` 
+
+bug: `language server not installed or missing from path`
 fix: `pnpm install -g diagnostic-languageserver`
+
+## key-bindings
+| key-bindings                                 | actions               |
+| :------------------------------------------- | :-------------------- |
+| <kbd>ctrl</kbd> + <kbd>b</kbd>               | cmp: scroll docs      |
+| <kbd>ctrl</kbd> + <kbd>f</kbd>               | cmp: scrol docs       |
+| <kbd>ctrl</kbd> + <kbd></kbd>                | space cmp: complete   |
+| <kbd>ctrl</kbd>                              | cmp: confirm          |
+| <kbd>ctrl</kbd>+ <kbd>space</kbd>            | cmp: complete         |
+| <kbd>ctrl</kbd>+ <kbd>p</kbd>                | telescope: find files |
+| <kbd>ctrl</kbd> + <kbd>f</kbd> <kbd>g</kbd>  | telescop: live grep   |
+| <kbd>ctrl</kbd> + <kbd>b</kbd>               |                       |
+| <kbd>ctrl</kbd> + <kbd>f</kbd>               |                       |
+| <kbd>ctrl</kbd> + <kbd>space</kbd>           |                       |
+| <kbd>ctrl</kbd>+ <kbd>e</kbd>                |                       |
+| <kbd>ctrl</kbd>                              |                       |
+| <kbd>space</kbd> + <kbd>d</kbd> <kbd>t</kbd> |                       |
+| <kbd>space</kbd> + <kbd>d</kbd> <kbd>c</kbd> |                       |
+| <kbd>space</kbd> + <kbd>d</kbd> <kbd>x</kbd> |                       |
+| <kbd>space</kbd> + <kbd>d</kbd> <kbd>o</kbd> |                       |
+| <kbd>shift</kbd>+ <kbd>k</kbd>               |                       |
+| <kbd>space</kbd> + <kbd>g</kbd> <kbd>d</kbd> |                       |
+| <kbd>space</kbd> + <kbd>g</kbd> <kbd>r</kbd> |                       |
+| <kbd>space</kbd> + <kbd>c</kbd> <kbd>a</kbd> |                       |
+| <kbd>space</kbd> + <kbd>f</kbd> <kbd>f</kbd> |                       |
+| <kbd>space</kbd> + <kbd>b</kbd> <kbd>f</kbd> |                       |
+| <kbd>space</kbd> + <kbd>g</kbd> <kbd>f</kbd> |                       |
+
+
+
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,61 +1,39 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "cssmodules_ls",
-          "cssls",
-          "golangci_lint_ls",
-          "gopls",
-          "html",
-          "jsonls",
-          "biome",
-          "marksman",
-          "prismals",
-          "pyre",
-          "pyright",
-          "tailwindcss",
-          "taplo",
-          "hydra_lsp",
-          "vtsls"
-        }
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.cssmodules_ls.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.golangci_lint_ls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.biome.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.prismals.setup({})
-      lspconfig.pyre.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.tailwindcss.setup({})
-      lspconfig.taplo.setup({})
-      lspconfig.hydra_lsp.setup({})
-      lspconfig.vtsls.setup({})
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
